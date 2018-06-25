b08ef6f4b11d40c87bc6725fc73022e8c02b9486
Dmitro Didenko <dmitrdida@ukr.net>
2018-06-25 10:14:05 +0300

    js_home-work-3

--- JavaScript/M-1/js/index.js
+++ JavaScript/M-1/js/index.js
@@ -371,7 +371,7 @@
 //     work++;
 // } while (work > 5);
 
-// ........................цикл for........................
+// ====================================== цикл for ======================================
 
 // let arr = [5, 10, 25, 68, 78, 95];
 // for ( let i = 0, max = arr.length; i < max; i++) {
@@ -527,6 +527,273 @@
 // }
 // console.log(arrnum);
 // console.log(sum);
+
+// ============================================== функции ==============================================
+//
+// function sum() {
+//     console.log(5 + 6)
+// }
+// sum();
+
+// function sum(x, y) {
+//     console.log(x + y)
+// }
+// sum(10,12);
+// sum(100,12);
+// sum(10,52);
+// sum(0,73);
+
+// function showText() {
+//     let str = 'I am function';
+//     return str;
+// }
+// console.log(showText());
+
+// function calculator(a, b) {
+//     let sum = a + b; // 15
+//     let mult = a * b; // 50
+//     let minus = a - b; // 5
+//     let div = a / b; // 2
+//     let result = sum + mult + minus + div; // 72
+//     return ['сума ' + sum, mult, minus, div, result];
+// }
+
+// function checkNumberType(num) {
+//     if (num % 2 === 1) {
+//         return 'odd'
+//     } else {
+//         return 'even'
+//     }
+//
+//     return num % 2 === 1 ? 'odd': 'even';
+// }
+//
+// console.log(checkNumberType(17));
+// console.log(checkNumberType(3));
+// console.log(checkNumberType(32));
+// console.log(checkNumberType(26));
+
+/*
+  Напишите функцию formatString(str)
+
+  - Функия принимает на вход строку str
+  - Если длина строки не превышает 40 символов, функция возвращает ее.
+  - Если длина больше 40 символов, то функция обрезает строку до 40-ти
+    символов и добавляет в конец строки троеточие '...', после чего
+    возвращает укороченную версию.
+*/
+
+// // Вызовы функции для проверки
+// console.log(
+//     formatString("Curabitur ligula sapien, tincidunt non.")
+// ); // вернется оригинальная строка
+//
+// console.log(
+//     formatString("Vestibulum facilisis, purus nec pulvinar iaculis.")
+// ); // вернется форматированная строка
+//
+// console.log(
+//     formatString("Curabitur ligula sapien.")
+// ); // вернется оригинальная строка
+//
+// console.log(
+//     formatString("Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.")
+// ); // вернется форматированная строка
+//
+// function formatString(text) {
+//     // if (text.length <= 40) {
+//     //     return text;
+//     // } else {
+//     //     return ${text.slice(0,40)}'...'
+//     // }
+//     return text <= 40 ? text: `${text.slice(0,40)}...`
+// }
+
+/*
+  Напишите функцию checkForSpam(str)
+
+  Функция принимает 1 параметр str - строку,
+  и проверять ее на содержание слов: spam и sale
+
+  Если нашли запрещенное слово то функция возвращает true,
+  если запрещенных слов нет функция возвращает false
+
+  PS: слова могут быть в произвольном регистре
+*/
+
+// // Вызовы функции для проверки
+// console.log(checkForSpam('Latest technology news')); // false
+//
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+//
+// console.log(checkForSpam('Get best sale offers now!')); // true
+//
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+//
+// function checkForSpam(str) {
+//     str = str.toLowerCase();
+//     if (str.indexOf('spam') === -1 && str.indexOf('sale') === -1) {
+//         return false;
+//     } else {
+//        return true;
+//     }
+//
+//     return !(str.indexOf('spam') === -1 && str.indexOf('sale') === -1);
+// }
+
+// function sum(a, b) {
+//     return a + b;
+// }
+// console.log(sum(2, 4));
+//
+// let sum1 = (a, b) => {
+//     return a + b
+// };
+// console.log(sum1(2, 4));
+//
+// let sum2 = (a, b) => a + b;
+// console.log(sum2(2, 4));
+
+/*
+  Написать функцию, getPx(str)
+
+  Функция getPx должна получать строку вида '10px',
+  проверять была ли передана строка, если да,
+  возвращать только числовую составляющую, к примеру 10.
+
+  Если была передана не строка, функция возвращает null.
+*/
+
+// Вызовы функции для проверки
+
+// function getPx(str) {
+//     if (typeof str === 'string') {
+//         return parseFloat(str);
+//     } else {
+//         return null
+//     }
+//     return typeof str === 'string' ? parseFloat(str) : null;
+// }
+
+// let getPx = (str) => typeof str === "string" ? parseFloat(str) : null;
+
+// console.log( getPx("10px") === 10 ); // должно быть:  true
+// console.log( getPx("10.5") === 10.5 ); // должно быть:  true
+// console.log( getPx("0") === 0 ); // должно быть:  true
+// console.log( getPx(-1) ); // должно быть:  null
+// console.log( getPx(10) ); // должно быть:  null
+
+/*
+  Создайте фукнцию findLongestWord(str),
+  которая получает аргументом произвольную строку и
+  возвращает самое длинное слово в этой строке.
+
+  Важное условие - в строке могут быть только пробелы
+  и символы букв и цифр!
+*/
+
+// function findLongestWord(str) {
+//     let arr = str.split(' ');
+//     let long = '';
+//     for (let el of arr) {
+//         el.length > long.length ? long = el : null;
+//     }
+//     return long
+// }
+//
+//
+// // Вызовы функции для проверки
+// console.log(
+//     findLongestWord("The quick brown fox jumped over the lazy dog")
+// ); // вернет 'jumped'
+//
+// console.log(
+//     findLongestWord("Google do a roll")
+// ); // вернет 'Google'
+//
+// console.log(
+//     findLongestWord("May the force be with you")
+// ); // вернет 'force'
+
+/*
+  Создайте функцию findLargestNumber(numbers),
+  которая получает массив чисел numbers, и возвращает
+  самое большое число в массиве.
+*/
+
+// function findLargestNumber(numbers) {
+//     let larg = 0;
+//     for (let el of numbers) {
+//        if (el >= larg) {
+//            larg = el
+//        }
+//     }
+//     return larg;
+// }
+
+// // Вызовы функции для проверки
+// console.log(
+//     findLargestNumber([1, 2, 3])
+// ); // вернет 3
+//
+// console.log(
+//     findLargestNumber([27, 12, 18, 5])
+// ); // вернет 27
+//
+// console.log(
+//     findLargestNumber([31, 128, 14, 74])
+// ); // вернет 128
+
+
+
+// 1) Написати функцію spliceElement() що вирізає з массива третій елемент
+// 2) Написати функцію checkExist() що перевіряє чи є цей вирізаний елемент в базі
+// 3) Якщо його немає додати його, якщо є нічого не робити addNumber()
+
+// let dataBase = [5, 10, 25, 68, 75, 43];
+// let workArr = [10, 20, 30, 40];
+//
+// function spliceElement(arr, position) {
+//     return arr.splice(position,1)[0];
+// }
+//
+// function checkExist(arr, number) {
+//     return arr.includes(number)
+// }
+//
+// function addNumber(arr, number) {
+//     let result = spliceElement(workArr, 2);
+//     if (!checkExist(arr, result)){
+//         arr.push(result);
+//     }
+// }
+// addNumber(dataBase);
+// console.log(dataBase);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
