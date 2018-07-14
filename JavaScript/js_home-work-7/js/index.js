a851b484a35c8d0b0b491572e3e2796fba07841d
Dmitro Didenko <dmitrdida@ukr.net>
2018-07-14 14:17:01 +0300

    js_home-work-7

--- JavaScript/js_home-work-7/js/index.js
+++ JavaScript/js_home-work-7/js/index.js
@@ -33,3 +33,53 @@ const posts = [
         link: 'link-3.com'
     }
 ];
+
+
+/*
+  Создайте функцию createPostCard(), которая
+  создает и возвращает DOM-узел карточки поста.
+
+  Разметка с классами есть на вкладке HTML.
+  Стили на вкладке CSS.
+
+  Используйте createElement для создания узлов.
+  Добавьте классы и атрибуты.
+*/
+
+const body = document.querySelector('body');
+
+function createPostCard() {
+    const div = document.createElement('div');
+    div.classList.add('post');
+
+    const img = document.createElement('img');
+    img.classList.add('post__image');
+    img.setAttribute('src', 'http://via.placeholder.com/400x150');
+    img.setAttribute('alt', 'post image');
+
+    const h2 = document.createElement('h2');
+    h2.classList.add('post__title');
+    h2.textContent = 'Lorem ipsum dolor';
+
+    const p = document.createElement('p');
+    p.classList.add('post__text');
+    p.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!';
+
+    const a = document.createElement('a');
+    a.classList.add('button');
+    a.textContent = 'Read more';
+    a.setAttribute('href', '#');
+    div.append(img, h2, p, a);
+
+    return div
+}
+
+function createCards(arr){
+    const div = document.createElement('div');
+    for(let el of arr){
+        div.append(createPostCard(el));
+    }
+    return div;
+}
+
+body.append(createCards(posts));
