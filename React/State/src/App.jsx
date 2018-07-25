4ad90b06e20e43805fea908bf162116d7571211d
Dmitro Didenko <dmitrdida@ukr.net>
2018-07-25 12:17:19 +0300

    12:17

---
+++ React/State/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import NewCounter from './Counter/Counter'
+import NewForm from './Counter/Form'
+
+const App = () => (
+  <div>
+    <h1>Main App component</h1>
+    {/*<NewCounter/>*/}
+    <NewForm/>
+  </div>
+);
+
+export default App
---
+++ React/State/src/Counter/Counter.jsx
@@ -0,0 +1,49 @@
+// import React from 'react';
+// // import PropTypes from 'prop-types'
+//
+// // import React, {Component} from 'react';  // другий спосіб
+// // class NewCounter extends Component // другий спосіб
+//
+// class NewCounter extends React.Component {
+//     // constructor() {
+//     //     super();
+//     //     this.state = {
+//     //         counterStart: 0,
+//     //     };
+//     //     this.plus = this.plus.bind(this);
+//     //     this.minus = this.minus.bind(this);
+//     // }
+//
+//     state = {
+//         counterStart: 0,
+//     };
+//
+//     // setState(obj або function(prevState), callback()) приймає обовязковим параметром функцію, обєкт.
+//     plus= () => {
+//         this.setState(prevState =>({counterStart: prevState.counterStart +1}))
+//     };
+//
+//     minus = () => {
+//         this.setState(prevState =>({counterStart: prevState.counterStart -1}))
+//     };
+//
+//     // static propTypes = {
+//     //     start: PropTypes.number,
+//     // };
+//     // static defaultProps = {
+//     //     start: 100,
+//     // };
+//
+//     render() {
+//         const {counterStart} = this.state;
+//         return (
+//             <div>
+//                 <p>{counterStart}</p>
+//                 <button onClick={this.plus}>Plus</button>
+//                 <button onClick={this.minus}>Minus</button>
+//             </div>
+//         )
+//     }
+// }
+//
+// export default NewCounter;
