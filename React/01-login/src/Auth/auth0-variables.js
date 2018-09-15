10d254d102d59d37009914b3bb23f19467961c53
Dmitro Didenko <dmitrdida@ukr.net>
2018-09-15 12:00:37 +0300

    new change

---
+++ React/01-login/src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+    domain: 'apipas.auth0.com',
+    clientId: '9DPQecsbebeok59I5oGiFcRuC72xsQeo',
+    callbackUrl: 'http://localhost:3000/callback'
+};
--- React/music/src/Components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
-import './Logo.css';
-
-const Logo = () => {
-    return (<NavLink to="/" className="logo"><h1 className="title">Logo</h1></NavLink>)
-};
-
-export default Logo;
